9ed09373b87c8b286ed560d4987a6b678c9e964a
Joshua Coles <josh@coles.to>
2024-07-24 17:25:43 +0100

    Rename files

--- bcf.rb
+++ bcf.rb
@@ -1,6 +1,6 @@
 require_relative './bcf/dsl'
-require_relative './bcf/typst_converter'
-require_relative './bcf/tir_json'
+require_relative './bcf/nderer'
+require_relative './bcf/on'
 require_relative './bcf/models'
 
 module BCF
