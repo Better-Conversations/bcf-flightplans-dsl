2885dd33d215d23b483a4bdb70f371144ac77970
Joshua Coles <josh@coles.to>
2024-07-24 21:22:58 +0100

    Stash

--- lib/bcf_flightplans.rb
+++
@@ -1,1 +0,0 @@
-require_relative './bcf/flight_plans'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require "bcf/flight_plans"
+require "bcf/flightplans"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
