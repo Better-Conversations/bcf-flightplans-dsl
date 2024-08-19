f61588710e4be5760fee8a303030a3482896a343
Joshua Coles <josh@coles.to>
2024-08-19 15:37:19 +0100

    Add a guard to ensure that typst is installed when using the TypstRenderer

--- lib/bcf/flight_plans/renderer.rb
+++ lib/bcf/flight_plans/renderer.rb
@@ -66,6 +66,10 @@ module BCF
     class TypstRenderContext < FormatRenderContext
       def initialize
         super('formats/typst', 'typ')
+
+        unless Open3.capture3('which typst')[2].success?
+          raise 'Typst is not installed. See https://github.com/typst/typst for instructions'
+        end
       end
 
       def render_flight_plan(flight_plan)
