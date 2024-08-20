f80efd9d724eed9fd3ea216a5be771ec7599bdb4
Simon Coles <simonc@amphora-research.com>
2024-08-20 18:57:12 +0100

    Outcome of `standardrb --fix`

--- lib/bcf/flight_plans/renderer.rb
+++ lib/bcf/flight_plans/renderer.rb
@@ -5,7 +5,7 @@ module BCF
         # First we group adjacent spoken items together
         groups = []
 
-        self.items.each do |item|
+        items.each do |item|
           if item.is_a?(Spoken)
             last_group = groups.last
 
@@ -50,12 +50,12 @@ module BCF
         return content if content.is_a? String
 
         Tilt.new(@root.join("render_content.#{@extension}.erb"))
-            .render(self, content: content)
+          .render(self, content: content)
       end
 
       def render_note(note)
         Tilt.new(@root.join("render_note.#{@extension}.erb"))
-            .render(self, note: note)
+          .render(self, note: note)
       end
 
       def render_markdown(md)
@@ -65,12 +65,12 @@ module BCF
 
     class TypstRenderContext < FormatRenderContext
       def initialize
-        super('formats/typst', 'typ')
+        super("formats/typst", "typ")
       end
 
-      def render_flight_plan(flight_plan, for_user:nil)
-        Tilt.new(self.root.join('entry_point.typ.erb'))
-            .render(self, flight_plan: flight_plan, for_user: for_user)
+      def render_flight_plan(flight_plan, for_user: nil)
+        Tilt.new(root.join("entry_point.typ.erb"))
+          .render(self, flight_plan: flight_plan, for_user: for_user)
       end
 
       # TODO: It might be good to isolate these into markdown files which we then read back to avoid escaping issues.
@@ -86,32 +86,30 @@ module BCF
       end
 
       def self.render_html_flight_plan(flight_plan)
-        Tilt.new('formats/html/table.html.erb')
-            .render(new, flight_plan: flight_plan)
+        Tilt.new("formats/html/table.html.erb")
+          .render(new, flight_plan: flight_plan)
       end
     end
 
     class FlightPlan
-      def render_pdf(output_path, debug_typst_path = nil,for_user:nil)
+      def render_pdf(output_path, debug_typst_path = nil, for_user: nil)
         typst_renderer = TypstRenderContext.new
-        
+
         Dir.mktmpdir do |dir|
           # Copy all files from ./typst to the temp directory
-          Dir.glob(File.join(typst_renderer.root, '*')).each do |file|
+          Dir.glob(File.join(typst_renderer.root, "*")).each do |file|
             FileUtils.cp(file, dir)
           end
 
-          typst_path = File.join(dir, 'output.typ')
-          typst_content = typst_renderer.render_flight_plan(self, for_user:for_user)
+          typst_path = File.join(dir, "output.typ")
+          typst_content = typst_renderer.render_flight_plan(self, for_user: for_user)
           File.write(typst_path, typst_content)
           system("typst c #{typst_path} #{output_path}")
         end
       end
 
       def write_json(output_path)
-        File.open(output_path, 'w') do |f|
-          f.write(self.to_json)
-        end
+        File.write(output_path, to_json)
       end
     end
   end
